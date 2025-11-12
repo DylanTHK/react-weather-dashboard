85ae05627e220d9e7d6cf9097db5a539c5284ca0
Dylan <dylantanhk@gmail.com>
2025-11-12 22:12:27 +0800

    implementing schema in api.ts and components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { useQuery } from "@tanstack/react-query"
 import { getWeather } from "./api"
+import Card from "./components/cards/Card";
+import DailyForecast from "./components/cards/DailyForecast";
 
 
 function App() {
-  const { data } = useQuery({
+  const { data, isLoading, error } = useQuery({
     queryKey: ['weather'],
     queryFn: () => getWeather({ lat: 10, lon: 20 })
   });
 
-  return <div>{JSON.stringify(data)}</div>;
+  if (isLoading) return <Card title="">Loading...</Card>;
+  if (error) return <Card title="">Error loading weather</Card>;
+  if (!data) return <Card title="">No data available</Card>;
+
+  return (
+    <div className="flex flex-col gap-8">
+      <Card title="Current Weather">
+        {JSON.stringify(data?.current).slice(0, 100)}
+      </Card>
+      <Card title="Hourly Forecast">
+        {JSON.stringify(data?.hourly).slice(0, 100)}
+      </Card>
+      <DailyForecast />
+    </div>
+  );
 }
 
 export default App

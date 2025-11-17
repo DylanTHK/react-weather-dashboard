c5cb431c9fc29bba5172caf9d43f86b3f2138403
Dylan <dylantanhk@gmail.com>
2025-11-17 23:48:11 +0800

    feature_card add currentweather component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { getWeather } from "./api"
 import Card from "./components/cards/Card";
 import DailyForecast from "./components/cards/DailyForecast";
 import HourlyForecast from "./components/cards/HourlyForecast";
+import CurrentWeather from "./components/cards/CurrentWeather";
 
 
 function App() {
@@ -17,9 +18,7 @@ function App() {
 
   return (
     <div className="flex flex-col gap-8">
-      <Card title="Current Weather">
-        {JSON.stringify(data?.current).slice(0, 100)}
-      </Card>
+      <CurrentWeather />
       <HourlyForecast />
       <DailyForecast />
     </div>
